48769a23f1ee9b87ade936a8df581da6be403aac
axiao35 <104530210+axiao35@users.noreply.github.com>
2022-04-27 18:11:13 -0400

    Add files via upload

---
+++ src/main/java/com/TDgame/WhatIsJdbc.java
@@ -0,0 +1,20 @@
+package com.TDgame;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class WhatIsJdbc {
+    public static void main(String[] args) throws SQLException {
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank_management", "root", "Bohong264664");
+        Statement s = c.createStatement();
+        ResultSet rs = s.executeQuery("select * from person");
+        while (rs.next()) {
+            System.out.println(rs.getString(1) + "  " + rs.getString(2));
+        }
+        c.close();
+    }
+}
+
